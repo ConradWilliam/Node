c3bbcef8683ad260d1d0fac6d63e629ab8ccd213
ConradWilliam <mcwilliam131@gmail.com>
2023-05-09 11:44:58 +0300

    Changes

--- url.js
+++ url.js
@@ -14,3 +14,28 @@ http.get(url, (res) => {
     console.log(data);
   });
 });
+
+const url = require('url');
+
+
+const URL_TO_PARSE = 'https://www.example.com/p/a/t/h?prop1=value1&prop2=value2';
+
+const myUrl = new URL('https://www.example.com/p/a/t/h?prop1=value1&prop2=value2');
+
+const hostname = myUrl.hostname;
+const pathname = myUrl.pathname;
+const searchParams = myUrl.searchParams;
+
+
+
+
+const querystring = require('querystring');
+
+
+const url = 'https://www.example.com/p/a/t/h?course=node&lesson=http';
+
+const queryToParse = url.split('?')[1];
+
+const parsedQuery = querystring.parse(queryToParse);
+parsedQuery.exercise = 'querystring';
+const modifiedQueryString = querystring.stringify(parsedQuery);
